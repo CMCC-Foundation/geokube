Fixed bug with resampled time units and removed the workaround to add bounds

geokube/core/field.py
@@ -898,7 +898,7 @@ class Field(Variable, DomainMixin):
             resample_kwargs.update({"closed": "right"})
             da = ds.resample(indexer={self.time.name: frequency}, **resample_kwargs)
             new_bnds = np.empty(
-                shape=(len(da.groups), 2), dtype=np.dtype("datetime64[m]")
+                shape=(len(da.groups), 2), dtype=np.dtype("datetime64[ns]")
             )
             for i, v in enumerate(da.groups.values()):
                 new_bnds[i] = [bnds.values[v].min(), bnds.values[v].max()]
@@ -909,7 +909,7 @@ class Field(Variable, DomainMixin):
         else:
             da = ds.resample(indexer={self.time.name: frequency}, **resample_kwargs)
             new_bnds = np.empty(
-                shape=(len(da.groups), 2), dtype=np.dtype("datetime64[m]")
+                shape=(len(da.groups), 2), dtype=np.dtype("datetime64[ns]")
             )
             for i, v in enumerate(da.groups.values()):
                 new_bnds[i] = [self.time.values[v].min(), self.time.values[v].max()]
@@ -919,11 +919,8 @@ class Field(Variable, DomainMixin):
              da,
              coords={f"{bnds_name}": ((self.time.name, "bnds"), new_bnds)},
         )
-        # NOTE: This is a workaround that should be replaced by setting the
-        # coordinate bounds after the field is created.
-        res.coords[self.time.name].encoding['bounds'] = bnds_name
         field = Field.from_xarray(res, ncvar=self.name)
-        # field.time.bounds = new_bnds
+        field.time.bounds = new_bnds
         field.domain.crs = self.domain.crs
         field.domain._type = self.domain._type
 

tests/core/test_field.py
@@ -1138,7 +1138,7 @@ def test_resample_without_original_bounds(era5_globe_netcdf):
     diff_ = np.diff(tp_r.time.values)
     assert np.all(diff_ == np.full_like(diff_, fill_value=diff))
     assert tp_r.time.bounds is not None
-    assert tp_r.time.bounds['time_bnds'].shape[0] == tp_r.time.values.shape[0]
+    assert tp_r.time.bounds['time_bounds'].shape[0] == tp_r.time.shape[0]
 
 
 def test_resample_with_original_bounds(era5_rotated_netcdf_wso):
@@ -1148,7 +1148,7 @@ def test_resample_with_original_bounds(era5_rotated_netcdf_wso):
     diff_ = np.diff(wso_r.time.values)
     assert np.all(diff_ == np.full_like(diff_, fill_value=diff))
     assert wso_r.time.bounds is not None
-    assert wso_r.time.bounds['time_bnds'].shape[0] == wso_r.time.shape[0]
+    assert wso_r.time.bounds['time_bounds'].shape[0] == wso_r.time.shape[0]
 
 
 def test_adding_time_bounds(era5_netcdf):