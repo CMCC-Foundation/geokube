0a7c6df599db40450c1436b6ee2ed63362e2e52c
MMStojiljkovic <ms@mmst.xyz>
2022-05-11 12:58:38 +0200

    Added other required shape files

--- tests/fixtures/shapes.py
+++ tests/fixtures/shapes.py
@@ -4,5 +4,5 @@ import pytest
 
 @pytest.fixture
 def it_shape_100_km():
-    path = "tests//resources//it_100km.shp"
-    yield gpd.read_file(path).to_crs(epsg=4326)['geometry']
+    path = "tests//resources//italy_shapefile//it_100km.shp"
+    yield gpd.read_file(path).to_crs(epsg=4326)["geometry"]
