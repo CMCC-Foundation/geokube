f658a47fe403d1047d536fe97fd2e99244cd3bdc
Jakub Walczak <jakub.walczak@cmcc.it>
2022-07-12 13:48:37 +0200

    Update datacube.py

--- geokube/core/datacube.py
+++ geokube/core/datacube.py
@@ -453,7 +453,7 @@ class DataCube(DomainMixin):
         self.to_netcdf(path)
         return path
 
-    def to_dict(self, unique_values) -> dict:
+    def to_dict(self, unique_values=False) -> dict:
         return {
             "domain": self.domain.to_dict(unique_values),
             "fields": list(self.fields.keys()),
