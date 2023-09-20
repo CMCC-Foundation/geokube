9e8f0f72c0534381efbe851a7e0f6ad6b464f452
MMStojiljkovic <ms@mmst.xyz>
2023-09-20 15:58:54 +0200

    Implemented operations for points and profiles

--- geokube/core/field.py
+++ geokube/core/field.py
@@ -1,4 +1,7 @@
 from collections.abc import Mapping, Sequence
+from datetime import date, datetime
+from numbers import Number
+from typing import Any, Self
 
 import dask.array as da
 import numpy as np
@@ -6,7 +9,7 @@ import numpy.typing as npt
 import pint
 import xarray as xr
 
-from . import domain as domain_, indexes
+from . import axis, domain as domain_, indexes
 
 
 class PointsField:
@@ -18,6 +21,8 @@ class PointsField:
         '__properties',
         '__encoding'
     )
+
+    _DOMAIN_TYPE = domain_.Points
 
     def __init__(
         self,
@@ -62,8 +67,14 @@ class PointsField:
             data_vars={self.__name: (('_points',), data_)},
             coords=domain._coords
         )
-        for axis_ in domain.coord_system.axes:
-            dset = dset.set_xindex(axis_, indexes.OneDimIndex)
+        coord_system = domain.coord_system
+        hor_axes = set(coord_system.spatial.crs.AXES)
+        for axis_ in coord_system.axes:
+            if axis_ not in hor_axes:
+                dset = dset.set_xindex(axis_, indexes.OneDimIndex)
+        dset = dset.set_xindex(
+            [axis.latitude, axis.longitude], indexes.TwoDimHorPointsIndex
+        )
         self.__data = dset
 
     @property
@@ -94,6 +105,81 @@ class PointsField:
     def data(self) -> pint.Quantity:
         return self.__data[self.__name].data
 
+    # TODO: Consider making this a method of a future base class.
+    # TODO: Replace `Any` with an appropriate `TypeVar` instance that
+    # represents all eligible types.
+    def _new_field(
+        self, new_data: xr.Dataset, result_type: type[Any] | None = None
+    ) -> Any:
+        field_type = type(self) if result_type is None else result_type
+        domain_type = field_type._DOMAIN_TYPE
+        name = self.__name
+        return field_type(
+            name=name,
+            domain=domain_type(
+                coords={
+                    axis_: coord.data
+                    for axis_, coord in new_data.coords.items()
+                },
+                coord_system=self.__domain.coord_system
+            ),
+            data=new_data[name].data
+        )
+
+    # Spatial operations ------------------------------------------------------
+
+    def bounding_box(
+        self,
+        south: Number | None = None,
+        north: Number | None = None,
+        west: Number | None = None,
+        east: Number | None = None,
+        bottom: Number | None = None,
+        top: Number | None = None
+    ) -> Self | PointsField:
+        h_idx = {
+            axis.latitude: slice(south, north),
+            axis.longitude: slice(west, east)
+        }
+        new_data = self.__data.sel(h_idx, method='nearest', tolerance=np.inf)
+        if not (bottom is None and top is None):
+            v_idx = {axis.vertical: slice(bottom, top)}
+            new_data = self._new_field(new_data)._data
+            new_data = new_data.sel(v_idx, method='nearest', tolerance=np.inf)
+        return self._new_field(new_data)
+
+    def nearest_horizontal(
+        self,
+        latitude: npt.ArrayLike | pint.Quantity,
+        longitude: npt.ArrayLike | pint.Quantity
+    ) -> Self:
+        idx = {axis.latitude: latitude, axis.longitude: longitude}
+        new_data = self.__data.sel(idx, method='nearest', tolerance=np.inf)
+        return self._new_field(new_data)
+
+    def nearest_vertical(
+        self, elevation: npt.ArrayLike | pint.Quantity
+    ) -> Self:
+        idx = {axis.vertical: elevation}
+        new_data = self.__data.sel(idx, method='nearest', tolerance=np.inf)
+        return self._new_field(new_data)
+
+    # Temporal operations -----------------------------------------------------
+
+    def time_range(
+        self,
+        start: date | datetime | str | None = None,
+        end: date | datetime | str | None = None
+    ) -> Self:
+        idx = {axis.time: slice(start, end)}
+        new_data = self.__data.sel(idx)
+        return self._new_field(new_data)
+
+    def nearest_time(self, time: date | datetime | str) -> Self:
+        idx = {axis.time: time}
+        new_data = self.__data.sel(idx, method='nearest', tolerance=None)
+        return self._new_field(new_data)
+
 
 class ProfileField:
     __slots__ = (
@@ -104,6 +190,8 @@ class ProfileField:
         '__properties',
         '__encoding'
     )
+
+    _DOMAIN_TYPE = domain_.Profile
 
     def __init__(
         self,
@@ -185,8 +273,15 @@ class ProfileField:
             data_vars={self.__name: (('_profiles', '_levels'), data_)},
             coords=domain._coords
         )
-        # for axis_ in domain.coord_system.axes:
-        #     dset = dset.set_xindex(axis_)
+        # TODO: Consider adding the index for vertical.
+        coord_system = domain.coord_system
+        spat_axes = set(coord_system.spatial.axes)
+        for axis_ in coord_system.axes:
+            if axis_ not in spat_axes:
+                dset = dset.set_xindex(axis_, indexes.OneDimIndex)
+        dset = dset.set_xindex(
+            [axis.latitude, axis.longitude], indexes.TwoDimHorPointsIndex
+        )
         self.__data = dset
 
     @property
@@ -216,3 +311,77 @@ class ProfileField:
     @property
     def data(self) -> pint.Quantity:
         return self.__data[self.__name].data
+
+    # TODO: Consider making this a method of a future base class.
+    # TODO: Replace `Any` with an appropriate `TypeVar` instance that
+    # represents all eligible types.
+    def _new_field(
+        self, new_data: xr.Dataset, result_type: type[Any] | None = None
+    ) -> Any:
+        field_type = type(self) if result_type is None else result_type
+        domain_type = field_type._DOMAIN_TYPE
+        name = self.__name
+        return field_type(
+            name=name,
+            domain=domain_type(
+                coords={
+                    axis_: coord.data
+                    for axis_, coord in new_data.coords.items()
+                },
+                coord_system=self.__domain.coord_system
+            ),
+            data=new_data[name].data
+        )
+
+    # Spatial operations ------------------------------------------------------
+
+    def bounding_box(
+        self,
+        south: Number | None = None,
+        north: Number | None = None,
+        west: Number | None = None,
+        east: Number | None = None,
+        bottom: Number | None = None,
+        top: Number | None = None
+    ) -> Self:
+        h_idx = {
+            axis.latitude: slice(south, north),
+            axis.longitude: slice(west, east)
+        }
+        new_data = self.__data.sel(h_idx, method='nearest', tolerance=np.inf)
+        if not (bottom is None and top is None):
+            raise NotImplementedError()
+        return self._new_field(new_data)
+
+    def nearest_horizontal(
+        self,
+        latitude: npt.ArrayLike | pint.Quantity,
+        longitude: npt.ArrayLike | pint.Quantity
+    ) -> Self:
+        idx = {axis.latitude: latitude, axis.longitude: longitude}
+        new_data = self.__data.sel(idx, method='nearest', tolerance=np.inf)
+        return self._new_field(new_data)
+
+    def nearest_vertical(
+        self, elevation: npt.ArrayLike | pint.Quantity
+    ) -> Self:
+        raise NotImplementedError()
+        idx = {axis.vertical: elevation}
+        new_data = self.__data.sel(idx, method='nearest', tolerance=np.inf)
+        return self._new_field(new_data)
+
+    # Temporal operations -----------------------------------------------------
+
+    def time_range(
+        self,
+        start: date | datetime | str | None = None,
+        end: date | datetime | str | None = None
+    ) -> Self:
+        idx = {axis.time: slice(start, end)}
+        new_data = self.__data.sel(idx)
+        return self._new_field(new_data)
+
+    def nearest_time(self, time: date | datetime | str) -> Self:
+        idx = {axis.time: time}
+        new_data = self.__data.sel(idx, method='nearest', tolerance=None)
+        return self._new_field(new_data)
