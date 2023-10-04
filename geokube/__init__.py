a2c1cb304dc95ecfc46b8c1b7901a331bd2cd312
MMStojiljkovic <ms@mmst.xyz>
2023-10-04 15:31:44 +0200

    Added drivers

--- geokube/__init__.py
+++ geokube/__init__.py
@@ -4,3 +4,6 @@ from geokube.core.crs import Geodetic, RotatedGeodetic, Projection
 from geokube.core.domain import Grid, Points, Profile
 from geokube.core.field import GridField, PointsField, ProfileField
 from geokube.core.units import units
+from geokube.drivers.drivers import (
+    open_argo, open_eobs, open_era5, open_era5_it
+)
