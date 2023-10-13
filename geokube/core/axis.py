123db7e27cc34ab1c2a00e705db566d3f569002c
MMStojiljkovic <ms@mmst.xyz>
2023-10-13 10:06:55 +0200

    Modified Axis and derived types

--- geokube/core/axis.py
+++ geokube/core/axis.py
@@ -1,3 +1,6 @@
+from collections.abc import Mapping
+from typing import Any
+
 import numpy as np
 import numpy.typing as npt
 import pint
@@ -14,24 +17,35 @@ class Axis(str):
     # hashables as dimensions, the inheritance from `str` will not be
     # necessary any more. In that case, `Axis` might inherit from `Hashable`.
 
-    __slots__ = ('__default_units', '__dtype')
+    __slots__ = ('__default_units', '__dtype', '__encoding')
+
+    _DEFAULT_UNITS: pint.Unit
+    _DEFAULT_DTYPE: np.dtype
 
     def __new__(
-        cls, name: str, default_units: pint.Unit | str, dtype: npt.DTypeLike
+        cls,
+        default_units: pint.Unit | str | None = None,
+        dtype: npt.DTypeLike | None = None,
+        encoding: Mapping[str, Any] | None = None
     ):
-        return str.__new__(cls, name)
+        return str.__new__(cls, cls.__name__)
 
     def __init__(
-        self, name: str, default_units: pint.Unit | str, dtype: npt.DTypeLike
+        self,
+        default_units: pint.Unit | str | None = None,
+        dtype: npt.DTypeLike | None = None,
+        encoding: Mapping[str, Any] | None = None
     ) -> None:
-        self.__dtype = np.dtype(dtype)
-        self.__default_units = pint.Unit(default_units)
+        self.__dtype = np.dtype(dtype) or self._DEFAULT_DTYPE
+        self.__default_units = pint.Unit(default_units or self._DEFAULT_UNITS)
+        self.__encoding = {} if encoding is None else dict(encoding)
 
     def __repr__(self) -> str:
-        name = f"name='{self}'"
+        cls_ = self.__class__.__name__
         default_units = f"default_units='{self.__default_units}'"
         dtype = f"dtype='{self.__dtype}'"
-        return f"{self.__class__.__name__}({name}, {default_units}, {dtype})"
+        encoding = f"encoding={self.__encoding}"
+        return f"{cls_}({default_units}, {dtype}, {encoding})"
 
     @property
     def name(self) -> str:
@@ -45,37 +59,84 @@ class Axis(str):
     def dtype(self) -> np.dtype:
         return self.__dtype
 
+    @property
+    def encoding(self) -> dict[str, Any]:
+        return self.__encoding
+
 
 class Spatial(Axis):
-    pass
+    _DEFAULT_UNITS = units['meter']
+    _DEFAULT_DTYPE = np.dtype('float64')
 
 
 class Horizontal(Spatial):
     pass
 
 
+class Longitude(Horizontal):
+    _DEFAULT_UNITS = units['degrees_north']
+
+
+class Latitude(Horizontal):
+    _DEFAULT_UNITS = units['degrees_east']
+
+
+class GridLongitude(Horizontal):
+    _DEFAULT_UNITS = units['degrees']
+
+
+class GridLatitude(Horizontal):
+    _DEFAULT_UNITS = units['degrees']
+
+
+class X(Horizontal):
+    _DEFAULT_UNITS = units['meter']
+
+
+class Y(Horizontal):
+    _DEFAULT_UNITS = units['meter']
+
+
 class Elevation(Spatial):
+    _DEFAULT_UNITS = units['meter']
+
+
+class Vertical(Elevation):
+    pass
+
+
+class Height(Elevation):
+    pass
+
+
+class Depth(Elevation):
+    pass
+
+
+class Z(Elevation):
     pass
 
 
 class Time(Axis):
-    pass
+    _DEFAULT_UNITS = units['']
+    _DEFAULT_DTYPE = np.dtype('datetime64')
 
 
 class UserDefined(Axis):
-    pass
+    _DEFAULT_UNITS = units['']
+    _DEFAULT_DTYPE = np.dtype('float64')
 
 
-x = Horizontal('x', units['meter'], np.float64)
-y = Horizontal('y', units['meter'], np.float64)
-z = Elevation('z', units['meter'], np.float64)
-grid_latitude = Horizontal('grid_latitude', units['degrees'], np.float64)
-grid_longitude = Horizontal('grid_longitude', units['degrees'], np.float64)
-latitude = Horizontal('latitude', units['degrees_north'], np.float64)
-longitude = Horizontal('longitude', units['degrees_east'], np.float64)
-vertical = Elevation('vertical', units['meter'], np.float64)
-time = Time('time', units[''], np.datetime64)
-timedelta = Time('timedelta', units[''], np.timedelta64)
+x = X()
+y = Y()
+z = Z()
+grid_latitude = GridLatitude()
+grid_longitude = GridLongitude()
+latitude = Latitude()
+longitude = Longitude()
+vertical = Vertical()
+time = Time()
+timedelta = Time(dtype=np.timedelta64)
 
 
 def create(
